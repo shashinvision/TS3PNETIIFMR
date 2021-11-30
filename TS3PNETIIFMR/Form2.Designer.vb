c198d8ebbfc9b5a7f7b73795cba12ea12f7f9fad
Felipe Mancilla <felipei.mancilla@gmail.com>
2021-11-30 10:35:09 -0300

    [FIX] Men

--- TS3PNETIIFMR/Form2.Designer.vb
+++ TS3PNETIIFMR/Form2.Designer.vb
@@ -22,17 +22,68 @@ Partial Class Form2
     'No lo modifique con el editor de código.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
+        Me.MenuStrip1 = New System.Windows.Forms.MenuStrip()
+        Me.IngresarDietaToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.MenúToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.InformeDeLaDietaToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.EnsambladoToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.MenuStrip1.SuspendLayout()
         Me.SuspendLayout()
+        '
+        'MenuStrip1
+        '
+        Me.MenuStrip1.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.IngresarDietaToolStripMenuItem})
+        Me.MenuStrip1.Location = New System.Drawing.Point(0, 0)
+        Me.MenuStrip1.Name = "MenuStrip1"
+        Me.MenuStrip1.Size = New System.Drawing.Size(778, 24)
+        Me.MenuStrip1.TabIndex = 0
+        Me.MenuStrip1.Text = "MenuStrip1"
+        '
+        'IngresarDietaToolStripMenuItem
+        '
+        Me.IngresarDietaToolStripMenuItem.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.MenúToolStripMenuItem, Me.InformeDeLaDietaToolStripMenuItem, Me.EnsambladoToolStripMenuItem})
+        Me.IngresarDietaToolStripMenuItem.Name = "IngresarDietaToolStripMenuItem"
+        Me.IngresarDietaToolStripMenuItem.Size = New System.Drawing.Size(50, 20)
+        Me.IngresarDietaToolStripMenuItem.Text = "Menú"
+        '
+        'MenúToolStripMenuItem
+        '
+        Me.MenúToolStripMenuItem.Name = "MenúToolStripMenuItem"
+        Me.MenúToolStripMenuItem.Size = New System.Drawing.Size(180, 22)
+        Me.MenúToolStripMenuItem.Text = "Ingresar Dieta"
+        '
+        'InformeDeLaDietaToolStripMenuItem
+        '
+        Me.InformeDeLaDietaToolStripMenuItem.Name = "InformeDeLaDietaToolStripMenuItem"
+        Me.InformeDeLaDietaToolStripMenuItem.Size = New System.Drawing.Size(180, 22)
+        Me.InformeDeLaDietaToolStripMenuItem.Text = "Informe de la Dieta"
+        '
+        'EnsambladoToolStripMenuItem
+        '
+        Me.EnsambladoToolStripMenuItem.Name = "EnsambladoToolStripMenuItem"
+        Me.EnsambladoToolStripMenuItem.Size = New System.Drawing.Size(180, 22)
+        Me.EnsambladoToolStripMenuItem.Text = "Ensamblado"
         '
         'Form2
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(7.0!, 15.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(778, 689)
+        Me.Controls.Add(Me.MenuStrip1)
+        Me.MainMenuStrip = Me.MenuStrip1
         Me.Name = "Form2"
         Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
         Me.Text = "Form2"
+        Me.MenuStrip1.ResumeLayout(False)
+        Me.MenuStrip1.PerformLayout()
         Me.ResumeLayout(False)
+        Me.PerformLayout()
 
     End Sub
+
+    Friend WithEvents MenuStrip1 As MenuStrip
+    Friend WithEvents IngresarDietaToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents MenúToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents InformeDeLaDietaToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents EnsambladoToolStripMenuItem As ToolStripMenuItem
 End Class
