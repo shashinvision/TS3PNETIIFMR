79659a097a1b9b5a17eb824fbbe573f8ef8ec71d
Felipe Mancilla <felipei.mancilla@gmail.com>
2021-11-30 10:26:11 -0300

    Agregar archivos de proyecto.

---
+++ TS3PNETIIFMR/Form1.vb
@@ -0,0 +1,7 @@
+﻿Public Class Form1
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Dim formHijo As New Form2
+        formHijo.MdiParent = Me
+        formHijo.Show()
+    End Sub
+End Class
