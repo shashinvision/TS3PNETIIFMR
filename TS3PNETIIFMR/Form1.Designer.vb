6c90947cdb26418ab9c37947356cdebee98c9df0
Felipe Mancilla <felipei.mancilla@gmail.com>
2021-11-30 10:52:32 -0300

    [ADD] informe Dieta

--- TS3PNETIIFMR/Form1.Designer.vb
+++ TS3PNETIIFMR/Form1.Designer.vb
@@ -22,7 +22,47 @@ Partial Class Form1
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
+        Me.MenuStrip1 = New System.Windows.Forms.MenuStrip()
+        Me.IngresarDietaToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.MenúToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.InformeDeLaDietaToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.EnsambladoToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.MenuStrip1.SuspendLayout()
         Me.SuspendLayout()
+        '
+        'MenuStrip1
+        '
+        Me.MenuStrip1.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.IngresarDietaToolStripMenuItem})
+        Me.MenuStrip1.Location = New System.Drawing.Point(0, 0)
+        Me.MenuStrip1.Name = "MenuStrip1"
+        Me.MenuStrip1.Size = New System.Drawing.Size(800, 24)
+        Me.MenuStrip1.TabIndex = 1
+        Me.MenuStrip1.Text = "MenuStrip1"
+        '
+        'IngresarDietaToolStripMenuItem
+        '
+        Me.IngresarDietaToolStripMenuItem.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.MenúToolStripMenuItem, Me.InformeDeLaDietaToolStripMenuItem, Me.EnsambladoToolStripMenuItem})
+        Me.IngresarDietaToolStripMenuItem.Name = "IngresarDietaToolStripMenuItem"
+        Me.IngresarDietaToolStripMenuItem.Size = New System.Drawing.Size(50, 20)
+        Me.IngresarDietaToolStripMenuItem.Text = "Menú"
+        '
+        'MenúToolStripMenuItem
+        '
+        Me.MenúToolStripMenuItem.Name = "MenúToolStripMenuItem"
+        Me.MenúToolStripMenuItem.Size = New System.Drawing.Size(180, 22)
+        Me.MenúToolStripMenuItem.Text = "Ingresar Dieta"
+        '
+        'InformeDeLaDietaToolStripMenuItem
+        '
+        Me.InformeDeLaDietaToolStripMenuItem.Name = "InformeDeLaDietaToolStripMenuItem"
+        Me.InformeDeLaDietaToolStripMenuItem.Size = New System.Drawing.Size(180, 22)
+        Me.InformeDeLaDietaToolStripMenuItem.Text = "Informe de la Dieta"
+        '
+        'EnsambladoToolStripMenuItem
+        '
+        Me.EnsambladoToolStripMenuItem.Name = "EnsambladoToolStripMenuItem"
+        Me.EnsambladoToolStripMenuItem.Size = New System.Drawing.Size(180, 22)
+        Me.EnsambladoToolStripMenuItem.Text = "Ensamblado"
         '
         'Form1
         '
@@ -31,12 +71,21 @@ Partial Class Form1
         Me.BackgroundImage = Global.TS3PNETIIFMR.My.Resources.Resources.dieta
         Me.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch
         Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.MenuStrip1)
         Me.IsMdiContainer = True
         Me.Name = "Form1"
         Me.Text = "Nutricionista Experto - Felipe Mancilla"
         Me.WindowState = System.Windows.Forms.FormWindowState.Maximized
+        Me.MenuStrip1.ResumeLayout(False)
+        Me.MenuStrip1.PerformLayout()
         Me.ResumeLayout(False)
+        Me.PerformLayout()
 
     End Sub
 
+    Friend WithEvents MenuStrip1 As MenuStrip
+    Friend WithEvents IngresarDietaToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents MenúToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents InformeDeLaDietaToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents EnsambladoToolStripMenuItem As ToolStripMenuItem
 End Class
--- TS3PNETIIFMR/Form1.vb
+++ TS3PNETIIFMR/Form1.vb
@@ -1,7 +1,13 @@
 ﻿Public Class Form1
-    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
-        Dim formHijo As New Form2
-        formHijo.MdiParent = Me
-        formHijo.Show()
+    Private Sub MenúToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles MenúToolStripMenuItem.Click
+        Dim formIngresarDieta As New Form2
+        formIngresarDieta.MdiParent = Me
+        formIngresarDieta.Show()
+    End Sub
+
+    Private Sub InformeDeLaDietaToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles InformeDeLaDietaToolStripMenuItem.Click
+        Dim formInformeDieta As New Form3
+        formInformeDieta.MdiParent = Me
+        formInformeDieta.Show()
     End Sub
 End Class
